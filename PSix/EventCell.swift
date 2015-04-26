9d2b4a68496e06d4a1a89be99ef9776f28df484c
Avihu Turzion <avihu.turzion@gmail.com>
2015-04-26 16:38:50 +0300

    `EventCell` now displays event day and hour

--- PSix/EventCell.swift
+++ PSix/EventCell.swift
@@ -12,10 +12,11 @@ class EventCell: UITableViewCell {
 
     @IBOutlet weak var eventNameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var paymentCollectionStatusLabel: UILabel!
+    @IBOutlet weak var eventDateDayAndTime: UILabel!
     @IBOutlet weak var eventDateMonthLabel: UILabel!
     @IBOutlet weak var eventDateDayLabel: UILabel!
     @IBOutlet weak var eventThumbImage: UIImageView!
+    @IBOutlet weak var paymentCollectionStatus: UILabel!
     
     override func awakeFromNib() {
         eventNameLabel.adjustsFontSizeToFitWidth = false
@@ -28,6 +29,22 @@ class EventCell: UITableViewCell {
             locationLabel.text = event?.location
             eventDateMonthLabel.text = event?.startTime?.monthShortName.uppercaseString
             eventDateDayLabel.text = event?.startTime?.dayDoubleDigit
+            setDayAndTimeLabel()
+        }
+    }
+    
+    private func setDayAndTimeLabel() {
+        if let startDate = event?.startTime {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "EEEE, h:mm"
+            let extendedText = dateFormatter.stringFromDate(startDate) as NSString
+            let extTextSize = extendedText.sizeWithAttributes([NSFontAttributeName: eventDateDayAndTime.font])
+            if eventDateDayAndTime.bounds.width < extTextSize.width {
+                dateFormatter.dateFormat = "E, h:mm"
+                eventDateDayAndTime.text = dateFormatter.stringFromDate(startDate)
+            } else {
+                eventDateDayAndTime.text = extendedText as String
+            }
         }
     }
 
