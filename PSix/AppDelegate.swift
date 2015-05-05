8726de8de4ecddbbc421850ead2ebea7bda14fef
Avihu Turzion <avihu.turzion@gmail.com>
2015-05-05 15:58:24 +0300

    Reverted to have the window property

--- PSix/AppDelegate.swift
+++ PSix/AppDelegate.swift
@@ -12,6 +12,8 @@ import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         initModels()
